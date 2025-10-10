d5947d7a2c73cc830a2b7693ef1e68dbf3a4be65
Jain2975 <aarush2975@gmail.com>
2025-10-10 22:21:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -276,6 +276,7 @@ app.get("/algos/pdf",auth,async (req,res)=>{
       `;
       const browser = await puppeteer.launch({
         headless: true,
+        executablePath: "/usr/bin/google-chrome-stable", 
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
       });
 
